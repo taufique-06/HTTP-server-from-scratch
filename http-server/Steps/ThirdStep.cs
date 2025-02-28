e61b85abed988c4c39f26c13d272e1d2dde7002d
Taufique Azad <taufique.azad@mountainwarehouse.com>
2025-02-28 15:17:08 +0000

    Http server should be able to handle query params

--- http-server/Steps/ThirdStep.cs
+++ http-server/Steps/ThirdStep.cs
@@ -9,11 +9,11 @@ public class ThirdStep
     // Direct If the client requests /, you can respond with "Hello World".
     // Direct If the client requests /about, you can respond with "About Us".
     // Direct If the client requests a path that does not exist, return a 404 Not Found error.
-    
+
     private const string Post = "POST";
     private const string Get = "GET";
 
-    public string ReturnResponse(string method, int receivedBytes, byte[] buffer,string path)
+    public string ReturnResponse(string method, int receivedBytes, byte[] buffer, string path)
     {
         var responseContent = "";
 
@@ -22,9 +22,25 @@ public class ThirdStep
             if (path.Equals("/"))
             {
                 responseContent = "Helloo World!";
-            } else if (path.Equals("/contact"))
+            }
+            else if (path.StartsWith("/contact"))
             {
                 responseContent = "You can contact Taufique by emailing him";
+            }
+            else if (path.StartsWith("/search"))
+            {
+                //Query Parameter exists
+                var queries = ParseQueryParams(path);
+                
+                var query = queries.ContainsKey("query") && queries["query"].Count > 0 
+                    ? string.Join(", ", queries["query"])
+                    : "No query parameter found.";
+                
+                var page = queries.ContainsKey("page") && queries["page"].Count > 0 
+                    ? string.Join(", ", queries["page"]) 
+                    : "1";
+                
+                responseContent = $"GET: Search results for '{query}' (Page {page})";
             }
             else
             {
@@ -40,7 +56,7 @@ public class ThirdStep
                 {
                     data = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
                 }
-                
+
                 responseContent = "POST: Data received: " + data;
             }
         }
@@ -49,12 +65,39 @@ public class ThirdStep
             responseContent = "Other Method is not allowed at the moment. Work in Progress";
         }
 
-        
-        return 
+        return
             "HTTP/1.1 200 OK\r\n" +
             "Content-Type: text/plain; charset=UTF-8\r\n" +
             "Content-Length: " + responseContent.Length + "\r\n" +
             "\r\n" +
             responseContent;
+    }
+
+    private Dictionary<string, List<string>> ParseQueryParams(string url)
+    {
+        //URL = "/search?query=cat&page=2&query=dog&page=3"
+        var queryParams = new Dictionary<string, List<string>>();
+        var queryStart = url?.IndexOf("?") ?? -1;
+
+        if (queryStart != -1)
+        {
+            var queryString = url!.Substring(queryStart + 1); //query=cat&page=2&query=dog&page=3
+            var parameters = queryString.Split('&');
+
+            foreach (var param in parameters)
+            {
+                var keyValue = param.Split("=");
+                if (keyValue.Length == 2)
+                {
+                    if (!queryParams.ContainsKey(keyValue[0]))
+                    {
+                        queryParams[keyValue[0]] = new List<string>();
+                    }
+                    queryParams[keyValue[0]].Add(keyValue[1]);
+                }
+            }
+        }
+
+        return queryParams;
     }
 }
